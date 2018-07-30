9dcd493a8d2c593da862e5832c38ed6ee52ac06a
Zeqing Li <zeqingli92@gmail.com>
2018-07-29 21:44:01 -0500

    adding logo and toolbars

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Make webpack aware of the image and bundle to the destination.
+import burgerLogo from '../../assets/images/burger-logo.png'
+import classes from './Logo.css'
+
+const logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={burgerLogo} alt="MyBurger" />
+    </div>
+);
+
+export default logo;
