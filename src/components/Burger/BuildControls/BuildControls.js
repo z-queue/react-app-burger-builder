0cc26137f1c6c577ce96c996b53662708dd1cd9d
Zeqing Li <zeqingli92@gmail.com>
2018-07-31 22:13:38 -0500

    added build control buttons

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './buildControls.css';
+
+const buildControls = (props) => {
+  return (
+    <div className={classes.BuildControls}></div>
+  );
+}
+
+export default buildControls;
