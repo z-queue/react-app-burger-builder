de8bf22f240c826f5ff7b142876767182ea690b5
Zeqing Li <zeqingli92@gmail.com>
2018-08-04 09:13:59 -0500

    135. added responsive adjustment

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -11,7 +11,7 @@ const layout = (props) => (
     <SideDrawer />
     <main className={classes.Content}>
       {props.children}
-    </main>
+    </main> 
   </Aux>
 );
 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,7 +7,9 @@ import NavItems from '../NavigationItems/NavigationItems';
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo />
+    <div className={classes.Logo}>
+      <Logo />  
+    </div>
     <nav> 
       <NavItems />
     </nav>
