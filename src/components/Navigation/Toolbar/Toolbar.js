51a3b3a62f399ca1f8577ca36a0eb8825d46807e
Zeqing Li <zeqingli92@gmail.com>
2018-08-04 01:26:56 -0500

    133. added nav items to nav bar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
     <Logo />
-    <nav> ... </nav>
+    <nav> 
+      <NavItems />
+    </nav>
 </header>
 );
 
