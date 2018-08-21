1f84c5e231cddadd98630918d433bb412e27e853
Zeqing Li <zeqingli92@gmail.com>
2018-08-20 21:32:55 -0500

    added react-router-dom dependency & set up route and router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const app = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
