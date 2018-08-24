6908d28ccafe25d382aaf2285b303fb321ce62fd
Zeqing Li <zeqingli92@gmail.com>
2018-08-24 01:08:23 -0500

    added contact components

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData'
 
 class Checkout extends Component {
   state = {
@@ -35,8 +37,8 @@ class Checkout extends Component {
         <CheckoutSummary 
           ingredients={this.state.ingredients} 
           checkoutCancelled={this.checkoutCancelledHandler}
-          checkoutContinued={this.checkoutContinuedHandler}
-        />
+          checkoutContinued={this.checkoutContinuedHandler}/>
+        <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
       </div>
     );
   }
