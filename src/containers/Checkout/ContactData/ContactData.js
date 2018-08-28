7fc102530f7f1e6a7aac7bf13ed1ea555ec291b2
Zeqing Li <zeqingli92@gmail.com>
2018-08-27 23:55:52 -0500

    added order submission details

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -13,6 +13,15 @@ class ContactData extends Component {
       postalCode: ''
     }
   }
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    console.log(this.props.ingredients);
+    
+  }
+  
+
+
   render() {
     return (
       <div className={classes.ContactData}>
@@ -22,7 +31,7 @@ class ContactData extends Component {
           <input className={classes.Input} type="email" name="email" placeholder="Your Mail"/>
           <input className={classes.Input} type="text" name="street" placeholder="Street"/>
           <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"/>
-          <Button btnType="Success">ORDER</Button>
+          <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>
       </div>
     );
