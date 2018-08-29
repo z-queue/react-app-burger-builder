f24c9188f313dad9fc966ab0d124baaf88c45903
Zeqing Li <zeqingli92@gmail.com>
2018-08-29 00:02:37 -0500

    finished checkout page

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 
 class ContactData extends Component {
@@ -36,6 +37,7 @@ class ContactData extends Component {
     axios.post('/orders.json', order)
       .then(res => {
         this.setState( {loading : false} );
+        this.props.history.push('/')
       })
       .catch(err => {
         this.setState( {loading : false} );
@@ -46,16 +48,21 @@ class ContactData extends Component {
 
 
   render() {
+    let form = (
+      <form>
+        <input className={classes.Input} type="text" name="name" placeholder="Your Name"/>
+        <input className={classes.Input} type="email" name="email" placeholder="Your Mail"/>
+        <input className={classes.Input} type="text" name="street" placeholder="Street"/>
+        <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"/>
+        <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+      </form>);
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
     return (
       <div className={classes.ContactData}>
         <h4>Enter Your Contact Data</h4>
-        <form>
-          <input className={classes.Input} type="text" name="name" placeholder="Your Name"/>
-          <input className={classes.Input} type="email" name="email" placeholder="Your Mail"/>
-          <input className={classes.Input} type="text" name="street" placeholder="Street"/>
-          <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"/>
-          <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-        </form>
+        {form}
       </div>
     );
   }
